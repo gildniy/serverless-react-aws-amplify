94f727360cb45a1dd31ba72b2a4ab77eb1099fbf
Gildas Niyigena <gildniy05@gmail.com>
2019-09-05 22:19:24 +0200

    (feature) display data with React

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,57 @@
-import React from 'react';
-
+// @ts-ignore
+import React, { useState }   from 'react';
+// @ts-ignore
 import { withAuthenticator } from 'aws-amplify-react'
 
 const App: React.FC = () => {
 
-    state = {
-        notes: [
-            {
-                id: 1,
-                note: "Hello world!"
-            }
-        ]
-    };
+    interface State {
+        notes: {
+            id: number,
+            note: string
+        }[]
+    }
 
-    const { notes } = this.state;
+    // eslint-disable-next-line
+    const [state, setState] = useState<State>({
+        notes: [{
+            id: 1,
+            note: "Hello world!"
+        }]
+    });
 
-  return (
-      <div>App</div>
-  );
+    const { notes } = state;
+
+    // @ts-ignore
+    return (
+        <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
+            <h1 className="code f2-l blue">Amplify Notetaker</h1>
+            <form className="mb3">
+                <input
+                    type="text"
+                    className="pa2 f4"
+                    placeholder="Write your note"
+                />
+                <button className="pa2 f4" type="submit">
+                    Add note
+                </button>
+            </form>
+            <div>
+                { notes.map((item: any) => (
+                    <div key={item.id} className="flex items-center">
+                        <li
+                            className="list pa1 f3"
+                        >
+                            { item.note }
+                        </li>
+                        <button className="bg-transparent bn f4">
+                            <span>&times;</span>
+                        </button>
+                    </div>
+                )) }
+            </div>
+        </div>
+    );
 };
 
 export default withAuthenticator(App, { includeGreetings: true });
