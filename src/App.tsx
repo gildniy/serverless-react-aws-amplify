009aab8da7124097dbcf7be8f5bb6087de78c17b
Gildas Niyigena <gildniy05@gmail.com>
2019-09-05 11:22:24 +0200

    Amplify configured to work with React.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
+import { withAuthenticator } from 'aws-amplify-react'
+
 const App: React.FC = () => {
+
+    state = {
+        notes: [
+            {
+                id: 1,
+                note: "Hello world!"
+            }
+        ]
+    };
+
+    const { notes } = this.state;
+
   return (
       <div>App</div>
   );
 };
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true });
